e03018ff5fbd6fdc42b868b63e5d4e690c98f17e
Mattijs Snepvangers <pegasus.ict@gmail.com>
2018-06-29 11:54:50 +0200

    wip

---
+++ clientconfig.bash
@@ -0,0 +1,72 @@
+#!/bin/bash
+############################################################################
+# Pegasus' Linux Administration Tools #	  apt cacher client install script #
+# (C)2017-2018 Mattijs Snepvangers	  #				 pegasus.ict@gmail.com #
+# License: MIT						  # Please keep my name in the credits #
+############################################################################
+START_TIME=$(date +"%Y-%m-%d_%H.%M.%S.%3N")
+# Making sure this script is run by bash to prevent mishaps
+if [ "$(ps -p "$$" -o comm=)" != "bash" ]; then bash "$0" "$@" ; exit "$?" ; fi
+# Make sure only root can run this script
+if [[ $EUID -ne 0 ]]; then echo "This script must be run as root" ; exit 1 ; fi
+echo "$START_TIME ## Starting PostInstall Process #######################"
+### FUNCTIONS ###
+init() {
+	################### PROGRAM INFO ##############################################
+	declare -gr PROGRAM_SUITE="Pegasus' Linux Administration Tools"
+	declare -gr SCRIPT="${0##*/}"
+	declare -gr SCRIPT_DIR="${0%/*}"
+	declare -gr SCRIPT_TITLE="APT Caching Client Install Script"
+	declare -gr MAINTAINER="Mattijs Snepvangers"
+	declare -gr MAINTAINER_EMAIL="pegasus.ict@gmail.com"
+	declare -gr COPYRIGHT="(c)2017-$(date +"%Y")"
+	declare -gr VERSION_MAJOR=0
+	declare -gr VERSION_MINOR=0
+	declare -gr VERSION_PATCH=0
+	declare -gr VERSION_STATE="PRE-ALPHA"
+	declare -gr VERSION_BUILD=20180528
+	declare -gr LICENSE="MIT"
+	###############################################################################
+	declare -gr PROGRAM="$PROGRAM_SUITE - $SCRIPT_TITLE"
+	declare -gr SHORT_VERSION="$VERSION_MAJOR.$VERSION_MINOR.$VERSION_PATCH-$VERSION_STATE"
+	declare -gr VERSION="Ver$SHORT_VERSION build $VERSION_BUILD"
+}
+
+prep() {
+	declare -g CACHE_SERVER="192.168.49.210"
+	import "BASH_FUNC_LIB/default.inc.bash"
+	create_dir "$LOG_DIR"
+	header
+	goto_base_dir
+	parse_ini $INI_FILE
+	get_args $@
+}
+
+import() {
+	local _FILE="$1"
+	if [[ -f "$_FILE" ]]
+	then
+		source "$_FILE"
+	else
+		crit_line "File $_FILE not found!"
+		exit 1
+	fi
+}
+
+### FUNCTIONS ##########
+insert_proxy() {
+	local _FILE="/etc/apt/apt.conf.d/00aptproxy"
+	local _LINE="Acquire::http::Proxy \"http://$CACHE_SERVER:3142\";"
+	echo "$_LINE" > "$_FILE"
+}
+
+main() {
+	insert_proxy
+
+
+}
+######### BOILERPLATE ##############
+
+init
+prep
+main
---
+++ serverconfig.bash
@@ -0,0 +1,66 @@
+#!/bin/bash
+############################################################################
+# Pegasus' Linux Administration Tools #	  apt cacher server install script #
+# (C)2017-2018 Mattijs Snepvangers	  #				 pegasus.ict@gmail.com #
+# License: MIT						  # Please keep my name in the credits #
+############################################################################
+START_TIME=$(date +"%Y-%m-%d_%H.%M.%S.%3N")
+# Making sure this script is run by bash to prevent mishaps
+if [ "$(ps -p "$$" -o comm=)" != "bash" ]; then bash "$0" "$@" ; exit "$?" ; fi
+# Make sure only root can run this script
+if [[ $EUID -ne 0 ]]; then echo "This script must be run as root" ; exit 1 ; fi
+echo "$START_TIME ## Starting PostInstall Process #######################"
+### FUNCTIONS ###
+init() {
+	################### PROGRAM INFO ##############################################
+	declare -gr PROGRAM_SUITE="Pegasus' Linux Administration Tools"
+	declare -gr SCRIPT="${0##*/}"
+	declare -gr SCRIPT_DIR="${0%/*}"
+	declare -gr SCRIPT_TITLE="APT Caching Server Install Script"
+	declare -gr MAINTAINER="Mattijs Snepvangers"
+	declare -gr MAINTAINER_EMAIL="pegasus.ict@gmail.com"
+	declare -gr COPYRIGHT="(c)2017-$(date +"%Y")"
+	declare -gr VERSION_MAJOR=0
+	declare -gr VERSION_MINOR=0
+	declare -gr VERSION_PATCH=0
+	declare -gr VERSION_STATE="PRE-ALPHA"
+	declare -gr VERSION_BUILD=20180528
+	declare -gr LICENSE="MIT"
+	###############################################################################
+	declare -gr PROGRAM="$PROGRAM_SUITE - $SCRIPT_TITLE"
+	declare -gr SHORT_VERSION="$VERSION_MAJOR.$VERSION_MINOR.$VERSION_PATCH-$VERSION_STATE"
+	declare -gr VERSION="Ver$SHORT_VERSION build $VERSION_BUILD"
+}
+
+prep() {
+	import "BASH_FUNC_LIB/default.inc.bash"
+	create_dir "$LOG_DIR"
+	header
+	goto_base_dir
+	parse_ini $INI_FILE
+	get_args $@
+	declare -gr AC_CFG="/etc/apt-cacher-ng/acng.conf"
+}
+
+import() {
+	local _FILE="$1"
+	if [[ -f "$_FILE" ]]
+	then
+		source "$_FILE"
+	else
+		crit_line "File $_FILE not found!"
+		exit 1
+	fi
+}
+
+main() {
+	### install apt-cacher
+	apt_inst apt-cacher-ng
+	edit_line_in_file "CacheDir: /var/cache/apt-cacher-ng" "CacheDir: /var/cache/apt-cacher-ng" "$AC_CFG"
+
+}
+
+##### BOILERPLATE #####
+init
+prep
+main
